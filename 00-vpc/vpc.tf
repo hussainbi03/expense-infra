3f6e8fce85046c2dcab95a83a8520135e45c723e
hussainbi03 <hussainbis03@gmail.com>
2025-06-02 15:03:12 +0530

    jenkins practice

--- 00-vpc/vpc.tf
+++ 00-vpc/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     #source = "../../terraform-aws-vpc"
-    source = "git::https://github.com/Hussain-bi-shaik/terraform-aws-vpc.git"
+    source = "git::https://github.com/Hussain-bi-shaik/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     environment = var.environment
     vpc_cidr = var.vpc_cidr
